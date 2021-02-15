50c61045b56dbf69e6a21adf7e6abd845ce8ea0c
Alejandro AS <soyalextreme@gmail.com>
2021-02-15 15:31:54 -0600

    Adding the first homework for the website. Adding the primero parcial directory Adding Class 1 and Class 2 notes and scripts.

---
+++ scripts/index.js
@@ -0,0 +1,21 @@
+// Estamos escribiendo un nuevo modulo para poder hacer la validacion de las compras.   
+
+const exampleName = "Alejandro Andrade Soriano";
+
+const sayHello = ( name ) => {
+    const result = `Hello! ${name}`;
+    console.log(result);
+}
+
+function main() {
+    // main function
+    sayHello(exampleName);
+}
+
+
+
+main();
+
+
+
+
